001871b40d2c9207515839f36062616abb89b287
Niclas Forsman <niclas.forsman@dias.fi>
2025-11-10 22:27:44 +0200

    Delete old files

--- src/vasco/services/railways.clj
+++
@@ -1,9 +0,0 @@
-(ns vasco.services.railways)
-
-(defn get [system]
-  {:plan/id :railways/get
-   :plan/effects
-   [{:effect/kind :db/get
-     :effect/data (:db system)}
-    {:effect/kind :time/lookup
-     :effect/data (:now-ld system)}]})
